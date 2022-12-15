e5dd19f9ab6be37df285dc0998c33c3f396b0a10
Michel Doumit <micheldoumit@Michels-MacBook-Pro.local>
2022-12-15 16:45:06 -0300

    declaring relationships

--- app/models/inventory_item.rb
+++ app/models/inventory_item.rb
@@ -1,2 +1,3 @@
 class InventoryItem < ApplicationRecord
+    belongs_to :product
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,6 @@
 class Order < ApplicationRecord
+    has_many :items
+    belongs_to :coupon, optional: true
+    belongs_to :credit_card_payment
+    belongs_to :shipping_address
 end
